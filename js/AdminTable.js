193f265320f58124cc9f00bfb7c4be4203fda3c6
Markus Häge <haemarkus@gmail.com>
2022-02-24 21:38:54 +0100

    added actions to table

--- js/AdminTable.js
+++ js/AdminTable.js
@@ -1,16 +1,40 @@
-let table = new DataTable('#table_id', {
-    // options
-});
+$(document).ready(function() {
+    let table = new DataTable('#table_id', {
+         "columnDefs": [ {
+            "targets": -1,
+            "data": null,
+            "defaultContent": "<button>Delete</button>"
+        },
+        {
+            "targets": -2,
+            "data": null,
+            "defaultContent": "<input type='checkbox' checked data-toggle='toggle'>Enable</input>"
+        }
+        ]
+    } );
+ 
+    $('#table_id tbody').on( 'click', 'button', function () {
+        table
+        .row( $(this).parents('tr') )
+        .remove()
+        .draw();
+    } );
 
-let rowNode = table
-        .row.add( [ 'Bier', 'Gold Ochsen' ] )
-        .draw()
+    $('#addRow').on('click', function(){
+        let product = document.getElementById("product").value;
+        let company = document.getElementById("company").value;
+        let units = document.getElementById("units").value;
+        let price = document.getElementById("price").value;
+    
+        table.row.add([product,company, units, price]).draw();
+    });
+} );
 
 function addRowToTable(){
-    alert("Clicked")
     let product = document.getElementById("product").value;
     let company = document.getElementById("company").value;
-
-    table.row.add([product,company]).draw();
+    let units = document.getElementById("units").value;
+    let price = document.getElementById("price").value;
+    table.row.add([product,company, units, price]).draw();
 
 }
